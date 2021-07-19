defd45f5ffe4cef5d9ee5c69131acd6cf1583caf
Dark Tornado <dt3141592@naver.com>
2021-07-20 07:22:53 +0900

    첫 번째 삽질 결과

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 © 2021 Dark Tornado, All rights reserved.
 
 * 대화 기능 제대로 다시 구현한다면서 삽질하는거 올리는 곳
+
+## 버전 1.0
+
+1. 수신된 채팅 내용을 중 명사만 추출
+1. 해당 명사들이 가장 많이 포함된 문장들 선정
+1. 그 문장들 중 편집 거리가 가장 짧은 문장들 선정
+1. 선정된 문장들 중 하나 전송
+
+* 사용된 채팅 데이터 : [Chatbot_data_for_Korean v1.0](https://github.com/songys/Chatbot_data) (MIT License)
+* 형태소 분석 Rest API : [open-korean-text](https://github.com/open-korean-text/open-korean-text) (Apache License 2.0)
+* 편집 거리 계산 모듈 : [levenshtein.js](https://gist.github.com/andrei-m/982927) (MIT License)
---
+++ SkynetTalk 1.0.js
@@ -0,0 +1,91 @@
+var ED = require('levenshtein');
+var chatData = [];
+
+/* Load chat data from .csv file */
+onInit = () => {
+    try {
+        Log.info('Loading started');
+        var data = FileStream.read('/sdcard/000/dev/ChatbotData.csv').split('\n');
+        for (var n = 1; n < data.length; n++) {
+            var datum = data[n].split(',');
+            chatData[datum[0]] = datum[1];
+        }
+        Log.info('Loading finished');
+    } catch (e) {
+        print(e);
+    }
+};
+
+response = (room, msg, sender, isGroupChat, replier) => {
+    if (msg.startsWith('/s ')) {
+        msg = msg.replace('/s ', '');
+        chatHook(msg, replier);
+    } else if (msg.startsWith('/ss ')) {
+        msg = msg.replace('/ss ', '');
+        chatHook(msg, replier, true);
+    }
+};
+
+chatHook = (msg, replier, tf) => {
+    /* Tokenize input */
+    var url = 'https://open-korean-text-api.herokuapp.com/tokenize?text=' + msg;
+    var data = org.jsoup.Jsoup.connect(url)
+        .ignoreContentType(true).ignoreHttpErrors(true).get().text();
+    data = JSON.parse(data);
+    var tokens = [];
+    for (var n = 0; n < data.tokens.length; n++) {
+        if (data.tokens[n].includes('(Noun')) tokens.push(data.tokens[n].split('(Noun')[0]);
+    }
+
+    /* Find answer */
+    var result = [];
+    var max = 1;
+    for (var key in chatData) {
+        var cnt = wordCheck(key, tokens);
+        if (cnt > max) {
+            max = cnt;
+            result = [];
+        }
+        if (cnt == max) {
+            result.push(key);
+        }
+    }
+
+    if (result.length == 0) {
+        replier.reply('적당한 채팅을 찾지 못했어요');
+        if (tf) replier.reply('input: ' + msg + '\ntokens: ' + tokens.join());
+    } else {
+        var min = ED.getEditDistance(msg, result[0]);
+        var index = 0;
+        for (var n = 1; n < result.length; n++) {
+            var tmp = ED.getEditDistance(msg, result[n]);
+            if (tmp < min) {
+                min = tmp;
+                index = n;
+            }
+        }
+        var res = result[index];
+        replier.reply(chatData[res]);
+        if (tf) replier.reply('input: ' + msg + '\ntokens: ' + tokens.join() + '\nQ: ' + res + '\nA: ' + chatData[res] + '\nED: ' + min);
+    }
+    return;
+    var s = [];
+    for (var n = 0; n < result.length; n++) {
+        s[n] = result[n] + ' -> ' + chatData[result[n]];
+    }
+    replier.reply('input: ' + msg + '\ntokens: ' + tokens.join() + '\n----------\n' + s.join('\n'));
+};
+
+wordCheck = (chat, tokens) => {
+    var cnt = 0;
+    for (var n = 0; n < tokens.length; n++) {
+        if (chat.includes(tokens[n])) cnt++;
+    }
+    return cnt;
+}
+
+new java.lang.Thread({
+    run: function() {
+        onInit();
+    }
+}).start();
